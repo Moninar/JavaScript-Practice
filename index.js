d290ada024ec13b20b622af6dcd47bb7f85be316
Moninar <33311166+Moninar@users.noreply.github.com>
2019-06-07 17:51:21 -0700

    variables, functions, hoisting, scope

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 
-function isPrime(n){
-    var divisor = 2;
-  
-    while (n > divisor){
-      if(n % divisor == 0){
-       return false; 
-      }
-      else
-        divisor++;
-    }
-    return true;
+const a = [1, 2, 3]
+const doubled = a.forEach((num, index) => a[index] = num = num * 2)
+console.log(a)
+
+const b = [1, 2, 3]
+const triple = b.map((num, index) => num * 3)
+console.log(triple)
+
+var log = () => console.log("document")
+log()
+
+let val = 10
+val = 11
+console.log(val)
+
+// variables & function hosting
+function myFunction() {
+  console.log(myVar)
+  var myVar = 1
+  console.log(myVar)
+  test()
+  function test() {
+    let t = 2
+    console.log(t)
   }
-  console.log(123);
+}
+myFunction()
+
+// returns from functions
+
+
